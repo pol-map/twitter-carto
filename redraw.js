987e9eef3ac38d5675884f814ad87f1f97a67b97
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2022-09-22 14:26:05 +0200

    fix redraw

--- redraw.js
+++ redraw.js
@@ -1,16 +1,30 @@
+import { network } from "./08_network.js";
 import { network_layout } from "./09_network_layout.js";
 
 const startingDate = new Date("2022-07-22")
 const endDate = new Date("2022-09-22")
 
 let date = startingDate
-while (endDate-date >= 0) {
+const redraw = function(){
 	let year = date.getFullYear()
 	let month = (1+date.getMonth()).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false})
 	let datem = (date.getDate()).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false})
-	console.log(`\n\n# COMPUTE NETWORK LAYOUT FOR ${year}-${month}-${datem} ##############################`)
+	console.log(`\n\n# COMPUTE NETWORK FOR ${year}-${month}-${datem} ##############################`)
 
-	network_layout(date)
+	network(date)
+		.then(result => {
+			if (result.success) {
+				console.info("# NETWORK SUCCESSFUL.",result.msg)
+			} else {
+				console.error("# NETWORK FAILED", result.msg)
+			}
+		}, error => {
+			console.error("# NETWORK ERROR", error)
+		})
+		.then(() => {
+			console.log("# NOW COMPUTE NETWORK LAYOUT ####################")
+			return network_layout(date)
+		})
 		.then(result => {
 			if (result.success) {
 				console.info("# NETWORK LAYOUT SUCCESSFUL.",result.msg)
@@ -20,8 +34,14 @@ while (endDate-date >= 0) {
 		}, error => {
 			console.error("# NETWORK LAYOUT ERROR", error)
 		})
-		
-	date.setDate(date.getDate() + 1);
+		.then(() => {
+			date.setDate(date.getDate() + 1);
+			if (endDate-date >= 0) {
+				return redraw()
+			} else {
+				console.log("\n\n# DONE. ###############################`)")
+			}
+		})
 }
-console.log("\n\n# DONE. #")
+redraw()
 
