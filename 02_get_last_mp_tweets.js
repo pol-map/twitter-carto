8d2e23df8ad38b6589f39a2136562e3decc4842b
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2022-09-22 10:53:32 +0200

    CLI mode for the scripts

--- 02_get_last_mp_tweets.js
+++ 02_get_last_mp_tweets.js
@@ -293,3 +293,19 @@ export async function get_last_mp_tweets(date) {
 	  }
 	}
 }
+
+// Command line arguments
+// Date argument
+let date = undefined
+const dateArgRegexp = /d(ate)?=([0-9]{4}\-[0-9]{2}\-[0-9]{2})/i
+process.argv.forEach(d => {
+	let found = d.match(dateArgRegexp)
+	if (found && found[2]) {
+		date = found[2]
+	}
+})
+// Auto mode (run the script)
+if (process.argv.some(d => ["a","-a","auto","-auto"].includes(d))) {
+	console.log("Run script"+((date)?(" on date "+date):("")))
+	get_last_mp_tweets(date)
+}
--- 06_get_political_tweets.js
+++ 06_get_political_tweets.js
@@ -325,3 +325,19 @@ export async function get_political_tweets(date) {
 	   return input;
 	};
 }
+
+// Command line arguments
+// Date argument
+let date = undefined
+const dateArgRegexp = /d(ate)?=([0-9]{4}\-[0-9]{2}\-[0-9]{2})/i
+process.argv.forEach(d => {
+	let found = d.match(dateArgRegexp)
+	if (found && found[2]) {
+		date = found[2]
+	}
+})
+// Auto mode (run the script)
+if (process.argv.some(d => ["a","-a","auto","-auto"].includes(d))) {
+	console.log("Run script"+((date)?(" on date "+date):("")))
+	get_political_tweets(date)
+}
