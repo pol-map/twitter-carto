c029cec81046dc37f664850d0e14942f8ce557f9
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2022-10-19 17:29:27 +0200

    Get tweet resources' text and media

--- 05B_resource_extract_text.js
+++ 05B_resource_extract_text.js
@@ -28,7 +28,7 @@ export async function resource_extract_text(date) {
 	  trace: 5,
 	};
 
-	const logLevel = "info"
+	const logLevel = "debug"
 
 	const logger = createLogger({
 		level: logLevel,
@@ -43,6 +43,8 @@ export async function resource_extract_text(date) {
 	logger.info('***** RUN SCRIPT ****');
 	console.log("Log level is", logLevel)
 	logger.info('Log level is '+logLevel);
+
+	const twitterClient = new Client(process.env.BEARER_TOKEN);
 
 	async function main() {
 		// We'll need yesterday's folder
@@ -114,11 +116,384 @@ export async function resource_extract_text(date) {
 		  });
 		}
 
-		// TODO:
 		// Extract the text content from Twitter resources
+  	const tweetsDir = `${thisFolder}/tweets`
+  	if (!fs.existsSync(tweetsDir)){
+		  fs.mkdirSync(tweetsDir);
+		}
+  	const mediaDir = `${thisFolder}/media`
+		if (!fs.existsSync(mediaDir)){
+		  fs.mkdirSync(mediaDir);
+		}
+  	let newResourcesTwitterFetched = []
+		let batches = []
+		let currentBatch = []
+		newResourcesTwitter.forEach(r => {
+			// Check if the tweet's content is already there
+			let fileName = `${tweetsDir}/${r.id}.json`
+			let tweetsResponse = undefined
+			if (fs.existsSync(fileName)){
+				// The data has been previously downloaded: load it.
+				let tweetsResponseRaw
+				try {
+					tweetsResponseRaw = fs.readFileSync(fileName);
+				} catch (error) {
+					console.log("Error", error)
+					logger
+						.child({ context: {res, page, fileName, error:error.message} })
+						.error(`JSON file read error. The data could not be recovered.`);
+					return new Promise((resolve, reject) => {
+						logger.once('finish', () => resolve({success:false, msg:`JSON file read error. The data could not be recovered.`}));
+						logger.end();
+				  });
+				}
+				if (tweetsResponseRaw) {
+					try {
+				  	tweetsResponse = JSON.parse(tweetsResponseRaw);
+				  	logger
+				  		.child({ context: {r, fileName, tweetsResponse} })
+							.trace(`Tweets data response retrieved from local cache ${fileName}`);
+			  	} catch (error) {
+						console.log("Error", error)
+						logger
+							.child({ context: {r, fileName, tweetsResponseRaw, error:error.message} })
+							.error(`JSON file cannot be parsed. The data could not be recovered.`);
+						tweetsResponse = undefined
+					}
+				}
+			}
+			// If the file does not exist or failed, query Twitter.
+			if (tweetsResponse === undefined) {
+				// Add the tweet to the batch of tweets to retrieve
+				currentBatch.push(r.id)
+				if (currentBatch.length >= 100) {
+					batches.push(currentBatch)
+					currentBatch = []
+				}
+			} else {
+				// Add the retrieved data to the list
+				newResourcesTwitterFetched.push(tweetsResponse)
+			}
+		})
+		if (currentBatch.length > 0) {
+			batches.push(currentBatch)
+		}
+		logger
+			.info(`${batches.length} batches built for ${newResourcesTwitter.length} resources (${newResourcesTwitterFetched.length} already downloaded).`);
 
-		// TODO:
-		// Fetch the text content from URL resources
+		if (batches.length > 0) {
+			for (let b in batches) {
+				const batch = batches[b]
+				const settings = {
+					"ids": batch.join(","),
+					"expansions":[
+						"attachments.poll_ids",
+						"attachments.media_keys",
+						"author_id",
+						"edit_history_tweet_ids",
+						"entities.mentions.username",
+						"geo.place_id",
+						"in_reply_to_user_id",
+						"referenced_tweets.id",
+						"referenced_tweets.id.author_id",
+					],
+					"media.fields":[
+						"duration_ms",
+						"media_key",
+						"preview_image_url",
+						"type",
+						"url",
+						"alt_text",
+						"variants",
+					],
+					"tweet.fields": [
+						"attachments",
+			      "author_id",
+			      "context_annotations",
+			      "conversation_id",
+			      "created_at",
+			      "entities",
+			      "id",
+			      "in_reply_to_user_id",
+			      "lang",
+			      "possibly_sensitive",
+			      "referenced_tweets",
+			      "reply_settings",
+			      "source",
+			      "text",
+			      "withheld",
+			    ],
+					"user.fields":[
+						"id",
+						"name",
+						"username",
+					],
+			  }
+				let tweetsFetched = await getTweets(settings)
+				// Save each tweet's content
+				for (let t in tweetsFetched.data) {
+					const tweet = tweetsFetched.data[t]
+					// Save
+					const fileName = `${tweetsDir}/${tweet.id}.json`
+					const tweetString = JSON.stringify(tweet)
+					try {
+						fs.writeFileSync(fileName, tweetString)
+					  logger
+							.child({ tweet:tweet })
+							.debug(`The tweeting file for tweet ${tweet.id} was saved successfully`);
+					} catch(error) {
+						logger
+							.child({ tweet:tweet })
+							.error(`The tweeting file for tweet ${tweet.id} could not be saved`);						
+					}
+				}
+				// Save the data about each media
+				if (tweetsFetched.includes && tweetsFetched.includes.media) {
+					for (let i in tweetsFetched.includes.media) {
+						const media = tweetsFetched.includes.media[i]
+						// Save
+						const fileName = `${mediaDir}/${media.media_key}.json`
+						const mediaString = JSON.stringify(media)
+						try {
+							fs.writeFileSync(fileName, mediaString)
+						  logger
+								.child({ media:media })
+								.debug(`The file for media ${media.media_key} was saved successfully`);
+						} catch(error) {
+							logger
+								.child({ media:media })
+								.error(`The file for media ${media.media_key} could not be saved`);						
+						}
+					}
+				}
+			}
+		}
+
+		/*for (let i = 0; i<newResourcesTwitter.length; i++) {
+			const res = newResourcesTwitter[i]
+
+			const maxPages = 10 // 100 users per page
+			let page = 0
+			let pageToken
+			while (page<maxPages) {
+				let queryMain
+				let fileHandle
+
+				if (res.type == "tweet") {
+					fileHandle = res.id
+					queryMain = `"${res.id}"`
+				} else if (res.type == "url") {
+					fileHandle = res.id.replace(/[^a-zA-Z0-9_-]/gi, '-').slice(0, 100)
+					queryMain = `url:"${res.id}"`
+				}
+
+				const fileName = `${broadcastingsDir}/${fileHandle}-${page.toLocaleString('en-US', {minimumIntegerDigits: 3, useGrouping: false})}.json`
+				
+				let tweetsResponse
+				// If the file exists, we just load it (recovery system)
+				if (fs.existsSync(fileName)){
+					let tweetsResponseRaw
+					try {
+						tweetsResponseRaw = fs.readFileSync(fileName);
+					} catch (error) {
+						console.log("Error", error)
+						logger
+							.child({ context: {res, page, fileName, error:error.message} })
+							.error(`JSON file read error. The data could not be recovered.`);
+						return new Promise((resolve, reject) => {
+							logger.once('finish', () => resolve({success:false, msg:`JSON file read error. The data could not be recovered.`}));
+							logger.end();
+					  });
+					}
+					if (tweetsResponseRaw) {
+						try {
+					  	tweetsResponse = JSON.parse(tweetsResponseRaw);
+					  	logger
+					  		.child({ context: {res, page, fileName, tweetsResponse} })
+								.trace(`Tweets data response retrieved from local cache ${fileName}`);
+				  	} catch (error) {
+							console.log("Error", error)
+							logger
+								.child({ context: {res, page, fileName, tweetsResponseRaw, error:error.message} })
+								.error(`JSON file cannot be parsed. The data could not be recovered.`);
+							tweetsResponse = undefined
+						}
+					}
+				}
+
+				// If the file does not exist or failed, query Twitter.
+				if (tweetsResponse === undefined) {
+					let yesterday = new Date(targetDate.getTime());
+					yesterday.setDate(targetDate.getDate() - 1);
+					const yyear = yesterday.getFullYear()
+					const ymonth = (1+yesterday.getMonth()).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false})
+					const ydatem = (yesterday.getDate()).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false})
+
+					const settings = {
+						"query": `(is:retweet OR is:reply OR is:quote) ${queryMain}`,
+						"sort_order":["recency"],
+						"expansions":"author_id",
+						"user.fields":[
+							"id",
+							"name",
+							"username",
+						],
+						"tweet.fields": [
+				      "created_at",
+				      "author_id",
+				      "conversation_id",
+				      "in_reply_to_user_id",
+				      "referenced_tweets",
+				      "attachments",
+				      "entities",
+				      "withheld",
+				      "possibly_sensitive",
+				      "lang",
+				      "reply_settings",
+				    ],
+				    "start_time": `${yyear}-${ymonth}-${ydatem}T00:00:00Z`,
+			    	"end_time": `${year}-${month}-${datem}T00:00:00Z`,
+				    "max_results": 100,
+				  }
+
+					tweetsResponse = await getSearchQueryTweets(settings, pageToken)
+					// Save response
+					const tweetsResponseString = JSON.stringify(tweetsResponse)
+					try {
+						fs.writeFileSync(fileName, tweetsResponseString)
+					  logger
+							.child({ context: {res, page, fileName} })
+							.debug(`The tweeting file for resource ${i},  ${truncate(res.id)}, page ${page} was saved successfully`);
+					} catch(error) {
+						logger
+							.child({ context: {res, page, fileName, error} })
+							.error(`The tweeting file for resource ${i},  ${truncate(res.id)}, page ${page} could not be saved`);						
+					}
+				}
+
+				// Process the results
+				if (tweetsResponse && tweetsResponse.data && tweetsResponse.data.length > 0) {
+					// As the ids are in the "includes" section, we need to index them
+					let userIndex = {}
+					try {
+						tweetsResponse.includes.users.forEach(u => {
+							userIndex[u.id] = u
+						})
+					} catch (error) {
+						console.log("Error", error)
+						logger
+							.child({ context: {res, page, usersResponse, error:error.message} })
+							.error(`An error occured when indexing the user data of the broadcasing of resource ${i}, ${truncate(res.id)}, page ${page}.`);
+					}
+					try {
+						harvestedTweetsCount += tweetsResponse.data.length
+						tweetsResponse.data.forEach(d => {
+							// Mentions
+							let mentions = {}
+							if (d.entities && d.entities.mentions){
+								d.entities.mentions.forEach(m => {
+									mentions[m.id] = true
+								})
+							}
+							if (d.in_reply_to_user_id) {
+								mentions[d.in_reply_to_user_id] = true
+							}
+							mentions = Object.keys(mentions)
+							if (mentions.length > 0) {
+								// Hashtags
+								let hashtags = {}
+								if (d.entities && d.entities.hashtags){
+									d.entities.hashtags.forEach(h => {
+										hashtags[h.tag] = true
+									})
+								}
+								hashtags = Object.keys(hashtags)
+
+								// Media
+								let media = {}
+								if (d.attachments && d.attachments.media_keys){
+									d.attachments.media_keys.forEach(mk => {
+										media[mk] = true
+									})
+								}
+								media = Object.keys(media)
+								
+								// Create row (object)
+								let broadcasting = {
+									broadcaster_id: userIndex[d.author_id].id,
+									broadcaster_name: userIndex[d.author_id].name,
+									broadcaster_username: userIndex[d.author_id].username,
+									// resource_group_main: res.group_main,
+									resource_groups: res.groups,
+									resource_id: res.id || res.url,
+									resource_type: res.type,
+									// resource_url: res.url,
+									tweet_mentions: JSON.stringify(mentions),
+									tweet_text: d.text,
+									tweet_hashtags: JSON.stringify(hashtags),
+									tweet_media: JSON.stringify(media),
+									tweet_lang: ""+d.lang,
+								}
+								broadcastings.push(broadcasting)
+							}
+						})
+					} catch (error) {
+						console.log("Error", error)
+						logger
+							.child({ context: {res, page, usersResponse, error:error.message} })
+							.error(`Broadcasting response data could not be processed for resource ${i}, ${truncate(res.id)}, page ${page}.`);
+					}
+				}
+				page++
+				if (tweetsResponse && tweetsResponse.meta && tweetsResponse.meta.next_token) {
+					pageToken = tweetsResponse.meta.next_token
+				} else break;
+			}
+
+			logger
+				.info(`Resource ${i+1} retrieved (${harvestedTweetsCount}/${maxTweets} tweets)`);
+
+			if (harvestedTweetsCount > maxTweets) {
+				logger
+					.info(`Daily tweet limit attained, stopping here. (${harvestedTweetsCount} tweets harvested)`);
+				break
+			}
+		}
+
+		logger
+			.child({ context: {broadcastingsLength:broadcastings.length } })
+			.info(`Broadcastings retrieved (${broadcastings.length})`);
+		
+		// Save broadcastings as CSV
+		const broadcastingsFile = `${thisFolder}/broadcastings.csv`
+		const broadcastingsString = d3.csvFormat(broadcastings)
+		try {
+		
+			fs.writeFileSync(broadcastingsFile, broadcastingsString)
+			logger
+				.child({ context: {broadcastingsFile} })
+				.info('Broadcastings file saved successfully');
+		  
+			return new Promise((resolve, reject) => {
+				logger.once('finish', () => resolve({success:true, msg:`${broadcastings.length} broadcastings saved successfully.`}));
+				logger.end();					
+		  });
+		} catch(error) {
+			
+			logger
+				.child({ context: {broadcastingsFile, error} })
+				.error('The broadcastings file could not be saved');
+			return new Promise((resolve, reject) => {
+				logger.once('finish', () => resolve({success:false, msg:`The broadcastings file could not be saved.`}));
+				logger.end();
+		  });
+
+		}
+*/
+
+
+
+		/*// Fetch the text content from URL resources
 		const fetchUrlSettings = ["fetch", "url", resFile_URL]
 		let fetchUrlDataString
 		try {
@@ -185,7 +560,7 @@ export async function resource_extract_text(date) {
 				logger.once('finish', () => resolve({success:false, msg:`The new URL resources with text file could not be saved.`}));
 				logger.end();
 			})
-		}
+		}*/
 
 
 		// TODO:
@@ -212,6 +587,42 @@ export async function resource_extract_text(date) {
 				.child({ context: {filePath, error:error.message} })
 				.error(`The file "${title}" could not be loaded`);
 		}
+	}
+
+	async function getTweets(settings) {
+		// Rate limit: 300 queries per 15 minutes. So we wait the right amount of time to throttle.
+		await new Promise(resolve => setTimeout(resolve, 15*60*1000/300))
+
+		try {
+
+	    let tweets
+    	tweets = await twitterClient.tweets.findTweetsById(settings);
+
+	    if (tweets.errors) {
+		    logger
+		  		.child({ context: {settings} })
+					.warn(`Errors returned for ${tweets.errors.length} tweets.`);    	
+	    }
+	    if (tweets.data) {
+		    logger
+		  		.child({ context: {settings} })
+					.info(`${tweets.data.length} tweets retrieved.`);
+	    } else {
+		    logger
+		  		.child({ context: {settings} })
+					.warn(`No tweets retrieved.`);
+	    }
+	    logger
+	  		.child({ context: {settings, tweets} })
+				.trace(`Tweets data response retrieved.`);
+	    return tweets || {}
+	  } catch (error) {
+	    console.log("Error", error)
+			logger
+				.child({ context: {settings, error:error.message} })
+				.error('The API call to retrieve tweets failed.');
+			return {}
+	  }
 	}
 
 	function minet(opts) {
