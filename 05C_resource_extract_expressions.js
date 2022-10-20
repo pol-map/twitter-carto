99f611cb856f5ad3f08113c031d17b38ee1fcedf
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2022-10-20 09:39:02 +0200

    Kickstart extract expressions

---
+++ 05C_resource_extract_expressions.js
@@ -0,0 +1,103 @@
+import { createLogger, format, transports } from "winston";
+import * as https from "https";
+import * as http from "http";
+import * as fs from "fs";
+import * as d3 from 'd3';
+import { Client, auth } from "twitter-api-sdk";
+import dotenv from "dotenv";
+import { spawn } from "child_process";
+
+dotenv.config();
+
+export async function resource_extract_expressions(date) {
+
+	const targetDate = ((date === undefined)?(new Date() /*Now*/):(new Date(date)))
+	const year = targetDate.getFullYear()
+	const month = (1+targetDate.getMonth()).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false})
+	const datem = (targetDate.getDate()).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false})
+	const thisFolder = `data/${year}/${month}/${datem}`
+
+	// Logger
+	// Inspiration: https://blog.appsignal.com/2021/09/01/best-practices-for-logging-in-nodejs.html
+	const logLevels = {
+	  fatal: 0,
+	  error: 1,
+	  warn: 2,
+	  info: 3,
+	  debug: 4,
+	  trace: 5,
+	};
+
+	const logLevel = "info"
+
+	const logger = createLogger({
+		level: logLevel,
+	  levels: logLevels,
+	  format: format.combine(format.timestamp(), format.json()),
+	  transports: [
+	  	new transports.Console(),
+	  	new transports.File({ filename: `${thisFolder}/05C_resource_extract_expressions.log` })
+	  ],
+	});
+
+	logger.info('***** RUN SCRIPT ****');
+	console.log("Log level is", logLevel)
+	logger.info('Log level is '+logLevel);
+
+	const twitterClient = new Client(process.env.BEARER_TOKEN);
+
+	async function main() {
+		// We'll need yesterday's folder
+		let yesterday = new Date(targetDate.getTime());
+		yesterday.setDate(targetDate.getDate() - 1);
+		const yyear = yesterday.getFullYear()
+		const ymonth = (1+yesterday.getMonth()).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false})
+		const ydatem = (yesterday.getDate()).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false})
+		const yesterdaysFolder = `data/${yyear}/${ymonth}/${ydatem}`
+
+		// Load resources with text
+		const resFile_agg = `${thisFolder}/resources_7days_aggregated_text.csv`
+		let resources = loadFile(resFile_agg, 'resources')
+
+		console.log(resources[0].text)
+
+
+		console.log("Done.")
+	}
+
+	return main();
+
+	function loadFile(filePath, title) {
+		try {
+			// Load file as string
+			const csvString = fs.readFileSync(filePath, "utf8")
+			// Parse string
+			const data = d3.csvParse(csvString);
+			logger
+				.child({ context: {filePath} })
+				.info(`File "${title}" loaded`);
+			return data
+		} catch (error) {
+			console.log("Error", error)
+			logger
+				.child({ context: {filePath, error:error.message} })
+				.error(`The file "${title}" could not be loaded`);
+		}
+	}
+}
+
+// Command line arguments
+// Date argument
+let date = undefined
+const dateArgRegexp = /d(ate)?=([0-9]{4}\-[0-9]{2}\-[0-9]{2})/i
+process.argv.forEach(d => {
+	let found = d.match(dateArgRegexp)
+	if (found && found[2]) {
+		date = found[2]
+	}
+})
+// Auto mode (run the script)
+if (process.argv.some(d => ["a","-a","auto","-auto"].includes(d))) {
+	console.log("Run script"+((date)?(" on date "+date):("")))
+	resource_extract_expressions(date)
+}
