a00b7d007f6a72c5072cc9a4b06ffba85a06e2c1
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2022-10-21 16:32:18 +0200

    Write legend for Twitter carto

--- 10B_render_legend_twitter.js
+++ 10B_render_legend_twitter.js
@@ -12,7 +12,162 @@ export async function render_legend_twitter(date) {
   const datem = (targetDate.getDate()).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false})
   const thisFolder = `data/${year}/${month}/${datem}`
 
+  let localeData, polAffData
+
   const width = 3590
+  const height = 3590
+
+  let canvas = createCanvas(width, height)
+  const ctx = canvas.getContext("2d")
+
+
+  const locale = getLocaleData()
+  
+  const xOffset = 12
+  // Draw the title and info
+  let y = 100
+  drawText(ctx, `${locale.legendTwitter.title} ${year}-${month}-${datem}`, xOffset, y, "start", "#303040", 0, "104px Raleway")
+  y += 24 // Margin
+  locale.legendTwitter.textRows.forEach(txt => {
+    y += 54
+    drawText(ctx, txt, xOffset, y, "start", "#303040", 0, "semibold 46px Raleway")
+  })
+  // Légende couleurs
+  y += 36
+  const squareSize = 48
+  const colorCode = getColorCode(targetDate)
+  colorCode.forEach(d => {
+    drawSquare(xOffset, y, squareSize, d.color)
+    drawText(ctx, d.name, xOffset+squareSize+12, y+46, "start", "#303040", 0, "bold 40px Raleway")
+    y += squareSize+12
+  })
+  // Footer
+  y = height - 36
+  drawText(ctx, locale.legendTwitter.footer, xOffset, y, "start", "#303040", 0, "56px Raleway")
+
+  // Save legend
+  const legendFilename = `${thisFolder}/Legend Twitter.png`
+  let imgd = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height)
+  const out = fs.createWriteStream(legendFilename)
+  const stream = canvas.createPNGStream()
+  stream.pipe(out)
+  out.on('finish', () => {
+    console.log("Legend saved.")
+  })
+
+  // Internal methods
+  function drawText(ctx, txt, x, y, textAlign, text_color, text_border_thickness, font) {
+    ctx.textAlign = textAlign || "start";
+    ctx.font = font
+    if (text_border_thickness > 0) {
+      ctx.lineWidth = text_border_thickness;
+      ctx.fillStyle = text_color;
+      ctx.strokeStyle = text_color;
+      ctx.fillText(
+        txt,
+        x,
+        y
+      );
+      ctx.strokeText(
+        txt,
+        x,
+        y
+      );
+    } else {
+      ctx.lineWidth = 0;
+      ctx.fillStyle = text_color;
+      ctx.fillText(
+        txt,
+        x,
+        y
+      );
+    }
+  }
+
+  function drawSquare(x, y, size, color) {
+    ctx.strokeStyle = "#303040";
+    ctx.lineCap="round";
+    ctx.lineJoin="round";
+    ctx.fillStyle = color;
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.fillRect(x, y, size, size);
+    ctx.rect(x, y, size, size);
+    ctx.stroke();
+  }
+
+  function getLocaleData() {
+    if (localeData === undefined) {
+      try {
+        // Load affiliations file as string
+        const localeDataJson = fs.readFileSync('locale.json', "utf8")
+
+        try {
+          localeData = JSON.parse(localeDataJson)
+          console.log('Locale loaded and parsed');
+
+          return localeData
+        } catch (error) {
+          console.error("Error: the locale file could not be parsed.", error)
+        }
+      } catch (error) {
+        console.error("Error: the locale file could not be loaded", error)
+      }
+    } else {
+      return localeData
+    }
+  }
+
+  function getPolAffData() {
+    if (polAffData === undefined) {
+      try {
+        // Load affiliations file as string
+        const polAffDataJson = fs.readFileSync('political_affiliations.json', "utf8")
+
+        try {
+          polAffData = JSON.parse(polAffDataJson)
+          console.log('Political affiliations loaded and parsed');
+
+          return polAffData
+        } catch (error) {
+          console.error("Error: the political affiliations file could not be parsed.", error)
+        }
+      } catch (error) {
+        console.error("Error: the political affiliations file could not be loaded", error)
+      }
+    } else {
+      return polAffData
+    }
+  }
+
+  function getColorCode(date){
+    const locale = getLocaleData()
+    const polAffData = getPolAffData()
+    let era
+    polAffData.eras.forEach(e => {
+      let sdate = new Date(e.startDate)
+      let edate = new Date(e.endDate)
+      if (sdate <= date && date <= edate ) {
+        era = e
+      }
+    })
+
+    if (era===undefined) {
+      console.error(`No corresponding era found in political affiliations file`);
+    } else {
+      let colorCode = []
+      era.affiliations.forEach(a => {
+        if (a.showInLegend) {
+          colorCode.push({
+            name: a.name,
+            color: a.color
+          })
+        }
+      })
+      colorCode.push({name:locale.misc.otherAffiliation, color: "#a4a4a4"})
+      return colorCode
+    }
+  }
 }
 
 // Command line arguments
