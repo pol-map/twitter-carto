5952efce070e5873ba2c6ef5383228f96d1f2235
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2022-10-23 10:33:51 +0200

    Test daily key resources extraction

--- test.js
+++ test.js
@@ -1,73 +1,244 @@
-import { frameBuilder as fb } from "./frame-builder.js";
-import { createCanvas, loadImage, ImageData } from "canvas"
-import * as HME from "h264-mp4-encoder";
+import { Command } from 'commander';
+import { createLogger, format, transports } from "winston";
 import * as fs from "fs";
+import * as d3 from 'd3';
+import dotenv from "dotenv";
 
-let settings = {}
-settings.sdate = "2022-10-10"
-settings.edate = "2022-10-22"
-settings.framesPerSecond = 30; // FPS (frame rate)
-settings.framesPerImage = 3; // How long in frames does each image stay. 1=quick, 15=slow.
-settings.filtering = {
-	shortName: "Lola",
-	filter: b => b.tweet_text.toLowerCase().indexOf("lola") >= 0
-}
+dotenv.config();
 
-const startDate = new Date(settings.sdate)
-const endDate = new Date(settings.edate)
+export async function whoSaysWhat(date) {
 
-let date = new Date(startDate)
+	const targetDate = ((date === undefined)?(new Date() /*Now*/):(new Date(date)))
+	const year = targetDate.getFullYear()
+	const month = (1+targetDate.getMonth()).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false})
+	const datem = (targetDate.getDate()).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false})
+	const thisFolder = `data/${year}/${month}/${datem}`
 
-let encoder
-HME.default.createH264MP4Encoder()
-	.then(enc => {
-		encoder = enc
-    // Must be a multiple of 2.
-    encoder.width = 3840;
-    encoder.height = 2160;
-    encoder.frameRate = settings.framesPerSecond;
-    encoder.quantizationParameter = 12 // Default 33. Higher means better compression, and lower means better quality [10..51].
-    encoder.initialize();
-  })
-  .then(encodeFrame)
+	// Logger
+	// Inspiration: https://blog.appsignal.com/2021/09/01/best-practices-for-logging-in-nodejs.html
+	const logLevels = {
+	  fatal: 0,
+	  error: 1,
+	  warn: 2,
+	  info: 3,
+	  debug: 4,
+	  trace: 5,
+	};
 
-async function encodeFrame() {
-	if (endDate-date >= 0) {
-		let year = date.getFullYear()
-		let month = (1+date.getMonth()).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false})
-		let datem = (date.getDate()).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false})
-  	let folder = `data/${year}/${month}/${datem}`
+	const logLevel = "trace"
 
-		console.log("\n# Add frames for "+folder)
+	const logger = createLogger({
+		level: logLevel,
+	  levels: logLevels,
+	  format: format.combine(format.timestamp(), format.json()),
+	  transports: [
+	  	new transports.Console(),
+	  	new transports.File({ filename: `${thisFolder}/test.log` })
+	  ],
+	});
 
-		let frameFile = await fb.build(
-			"broadcasting",
-			date,
-			{
-				dateRange: [startDate, endDate],
-				labels:false,
-				filtering:settings.filtering,
-				reuseIfExists:false,
-			}
-		)
-		console.log("Frame generated:",frameFile)
-		let frameImage = await loadImage(frameFile)
-		
-		let canvas = createCanvas(3840, 2160)
-		const ctx = canvas.getContext("2d")
-		ctx.drawImage(frameImage, 0, 0)
-		let imgd = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height)
-  	for (let i=0; i<settings.framesPerImage; i++) {
-  	  encoder.addFrameRgba(imgd.data);
+	logger.info('***** RUN SCRIPT ****');
+	console.log("Log level is", logLevel)
+	logger.info('Log level is '+logLevel);
+
+	async function main() {
+
+		// Load spat users
+		const usersFile = `${thisFolder}/network_nodes_spat.csv`
+		const users = loadFile(usersFile, 'network_nodes_spat')
+
+		// Load last broadcastings
+		const broadcastingsFile = `${thisFolder}/broadcastings.csv`
+		const broadcastings = loadFile(broadcastingsFile, 'broadcastings')
+
+		// Index broadcasted resources per user
+		let resByUserIndex = {}
+		broadcastings.forEach(b => {
+			let resources = resByUserIndex[b.broadcaster_username] || []
+			resources.push(b.resource_id)
+			resByUserIndex[b.broadcaster_username] = resources
+		})
+
+		// Store the count of resources per user as weight
+		users.forEach(u => {
+			u.resources = resByUserIndex[u.username] || []
+			u.weight = u.resources.length
+		})
+
+		// Find barycenter
+		let xTotal = 0
+		let yTotal = 0
+		let wTotal = 0
+		users.forEach(u => {
+			xTotal += (+u.x || 0) * u.weight
+			yTotal += (+u.y || 0) * u.weight
+			wTotal += u.weight
+		})
+		const barycenter = {x:xTotal/wTotal, y:yTotal/wTotal}
+
+		// Find max distance on x or y between any node and the barycenter
+		let dMax = 0
+		users.forEach(u => {
+			const x = +u.x || 0
+			const y = +u.y || 0
+			dMax = Math.max(dMax, Math.max(Math.abs(barycenter.x-x), Math.abs(barycenter.y-y)))
+		})
+		dMax += 1 // we take a bit larger to avoid problems
+
+		/// Rudimentary quad tree
+		// We very roughly aim for the equivalent of a 4x4 split of the broadcastings
+		const weightThreshold = 33000 / 16
+		let initQuad = {
+			x: barycenter.x - dMax,
+			y: barycenter.y - dMax,
+			w: 2*dMax,
+			users: users,
+			weight: wTotal,
 		}
-    date.setDate(date.getDate() + 1)
-    return encodeFrame()
-  } else {
-  	encoder.finalize();
-    let uint8Array = encoder.FS.readFile(encoder.outputFilename);
-    encoder.delete();
-    fs.writeFileSync(`data/${settings.filtering.shortName} from ${settings.sdate} to ${settings.edate}.mp4`, Buffer.from(uint8Array));
-    console.log("Done.")
-  }
+		let quadsToTest = [initQuad]
+		let finalQuads = []
+		while (quadsToTest.length>0) {
+			let quad = quadsToTest.pop()
+
+			if (quad.users.length > 0){
+				// Test
+				if (quad.weight < weightThreshold || quad.users.length == 1) {
+					// We'good! Keep that quad.
+					finalQuads.push(quad)
+				} else {
+					// We split!
+					// Create four empty quads
+					let quadNW = {
+						x: quad.x,
+						y: quad.y,
+						w: quad.w/2,
+						users: [],
+						weight: 0,
+					}
+					let quadNE = {
+						x: quad.x + quad.w/2,
+						y: quad.y,
+						w: quad.w/2,
+						users: [],
+						weight: 0,
+					}
+					let quadSE = {
+						x: quad.x + quad.w/2,
+						y: quad.y + quad.w/2,
+						w: quad.w/2,
+						users: [],
+						weight: 0,
+					}
+					let quadSW = {
+						x: quad.x,
+						y: quad.y + quad.w/2,
+						w: quad.w/2,
+						users: [],
+						weight: 0,
+					}
+					// Sort the users
+					let xMid = quad.x+quad.w/2
+					let yMid = quad.y+quad.w/2
+					quad.users.forEach(u => {
+						const x = +u.x || 0
+						const y = +u.y || 0
+						if (x < xMid) {
+							if (y < yMid) {
+								quadNW.users.push(u)
+							} else {
+								quadSW.users.push(u)
+							}
+						} else {
+							if (y < yMid) {
+								quadNE.users.push(u)
+							} else {
+								quadSE.users.push(u)
+							}
+						}
+					})
+					// Compute the weights and add the sub-quads to the queue
+					let subquads = [quadNW, quadNE, quadSE, quadSW]
+					subquads.forEach(q => {
+						q.weight = d3.sum(q.users.map(u => u.weight))
+						quadsToTest.push(q)
+					})
+				}
+			} // If no users, do nothing (discard the quad).
+		}
+
+		// Sort the resources for each quad
+		finalQuads.forEach(quad => {
+			let quadResIndex = {}
+			quad.users.forEach(u => {
+				u.resources.forEach(res => {
+					quadResIndex[res] = (quadResIndex[res] || 0) + 1
+				})
+			})
+			// Get the counts and sort
+			quad.resources = []
+			for (let res in quadResIndex) {
+				quad.resources.push({id:res, count:quadResIndex[res]})
+			}
+			quad.resources.sort(function(a,b){return b.count-a.count})
+		})
+
+		console.log("Quads", finalQuads.map((q,i) => `${i} - ${q.users.length} users - ${q.resources.length} resources - ${q.weight} weight`))
+
+		// Sort the top resources of each quad by count
+		let keyResourcesIndex = {}
+		finalQuads.forEach(quad => {
+			let res = quad.resources[0]
+			if (res) {
+				keyResourcesIndex[res.id] = (keyResourcesIndex[res.id] || 0) + res.count
+			}
+		})
+		let keyResources = []
+		for (let resId in keyResourcesIndex) {
+			keyResources.push({id:resId, count:keyResourcesIndex[resId]})
+		}
+		keyResources.sort(function(a,b){return b.count-a.count})
+		// console.log("Key resources", keyResources)
+
+		console.log("Done.")
+	}
+
+	return main();
+
+	function loadFile(filePath, title) {
+		try {
+			// Load file as string
+			const csvString = fs.readFileSync(filePath, "utf8")
+			// Parse string
+			const data = d3.csvParse(csvString);
+			logger
+				.child({ context: {filePath} })
+				.info(`File "${title}" loaded`);
+			return data
+		} catch (error) {
+			console.log("Error", error)
+			logger
+				.child({ context: {filePath, error:error.message} })
+				.error(`The file "${title}" could not be loaded`);
+		}
+	}
+
 }
 
+/// CLI logic
+let thisFile = "test.js" // Prevent the CLI logic to trigger when used as a module
+if (process.argv[1].split(/[/\\]/).pop()==thisFile) {
+	const program = new Command();
+	program
+		.name('frame-builder')
+		.description('Utility usable as a CLI. Build frames that can be made into a video.')
+	  .requiredOption('-a, --auto', 'Auto mode.')
+	  .option('-d, --date <date>', 'Date as "YYYY-MM-DD". Defaults to today.')
+	  .showHelpAfterError()
+	  .parse(process.argv);
+
+	const options = program.opts();
+
+	if (options.auto) {
+		await whoSaysWhat(options.date ? new Date(options.date) : new Date())
+	}
+}
