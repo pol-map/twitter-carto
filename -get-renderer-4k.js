787d4f3a8245a5c47fc1fdd83466b66e6bd67bca
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2022-10-26 07:24:16 +0200

    separate renderer

---
+++ -get-renderer-4k.js
@@ -0,0 +1,402 @@
+import Graph from "graphology";
+import gexf from "graphology-gexf";
+import * as fs from "fs";
+import { createCanvas, loadImage, ImageData } from "canvas"
+import * as d3 from 'd3';
+import dotenv from "dotenv";
+import * as StackBlur from "stackblur-canvas";
+
+dotenv.config();
+
+export function getRenderer4K() {
+  
+  /// RENDERER
+  let newRenderer = function(){
+
+    // NAMESPACE
+    var ns = {}
+
+    // Activate when using Node.js
+    ns._nodejs = true
+
+    /// Define renderer
+    ns.render = function(g, settings) {
+
+      settings = settings || {}
+      
+      ns.init(g, settings)
+
+      const overlayImage = ns.drawOverlayLayer(ns.settings)
+      
+      // Build final canvas
+      var renderingCanvas = ns.createCanvas()
+      renderingCanvas.getContext("2d").putImageData(overlayImage, 0, 0)
+      if (ns.settings.output_dpi == ns.settings.rendering_dpi) {
+        return renderingCanvas
+      }
+      var canvas = ns.createCanvas()
+      let outputWidth = Math.floor(ns.settings.image_width * ns.settings.output_dpi * 0.0393701 / ns.settings.tile_factor)
+      let outputHeight = Math.floor(ns.settings.image_height * ns.settings.output_dpi * 0.0393701 / ns.settings.tile_factor)
+      canvas.width = outputWidth
+      canvas.height = outputHeight
+      let ctx = canvas.getContext("2d")
+      ctx.imageSmoothingEnabled = true
+      ctx.imageSmoothingQuality = "high"
+      ctx.drawImage(renderingCanvas, 0, 0, outputWidth, outputHeight);
+      return canvas
+    }
+
+    // Render and return image data
+    ns.renderAndGetImgd = async function(g, settings) {
+      return new Promise(resolve => {
+        let canvas = ns.render(g, settings)
+        resolve(canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height))
+      })
+    }
+
+    /// Initialization
+    ns.init = function(g, settings) {
+      if (ns._initialized) { return }
+
+      ns.report("Initialization")
+
+      // Orientation & layout:
+      settings.flip_x = false
+      settings.flip_y = true
+      settings.rotate = 0 // In degrees, clockwise
+      settings.margin_top    =  2 // in mm
+      settings.margin_right  = 12 // in mm
+      settings.margin_bottom = 12 // in mm
+      settings.margin_left   =  2 // in mm
+
+      // Image size and resolution
+      settings.image_width = 304 // in mm. Default: 200mm (fits in a A4 page)
+      settings.image_height = 171
+      settings.output_dpi = 320.842 // Dots per inch.
+      settings.rendering_dpi = 320.842 // Default: same as output_dpi. You can over- or under-render to tweak quality and speed.
+
+      settings.tile_factor = 1 // Integer, default 1. Number of rows and columns of the grid of exported images.
+      settings.tile_to_render = [0, 0] // Grid coordinates, as integers
+      ns.settings = settings
+      ns.g = g.copy()
+
+      // Fix missing coordinates and/or colors:
+      //  some parts of the script require default values
+      //  that are sometimes missing. We add them for consistency.)
+      ns.addMissingVisualizationData()
+
+      // For commodity, rescale the network to canvas-related coordinates
+      ns.rescaleGraphToGraphicSpace(ns.settings)
+
+      ns._initialized = true
+    }
+
+
+
+
+    /// FUNCTIONS
+
+    ns.drawOverlayLayer = function() {
+      console.log("### YOU MUST OVERRIDE THIS METHOD IN THE RENDERER:")
+      console.log("### renderer.drawOverlayLayer")
+    }
+
+    ns.truncateWithEllipsis = function(string, n) {
+      if (n && n<Infinity) return string.substr(0,n-1)+(string.length>n?'…':'');
+      return string
+    }
+
+    ns.paintAll = function(ctx, color) {
+      ctx.beginPath()
+      ctx.rect(0, 0, ctx.canvas.width, ctx.canvas.height)
+      ctx.fillStyle = color
+      ctx.fill()
+      ctx.closePath()
+    }
+
+    ns.createCanvas = function() {
+      let dim = ns.getRenderingPixelDimensions()
+      let canvas
+      if (ns._nodejs) {
+        canvas = createCanvas(dim.w, dim.h) // Node version
+      } else {
+        canvas = document.createElement('canvas')
+      }
+      canvas.width = dim.w
+      canvas.height = dim.h
+      return canvas
+    }
+
+    ns.scaleContext = function(ctx) {
+      ctx.scale(ns.settings.tile_factor, ns.settings.tile_factor)
+      ctx.translate(
+        -ctx.canvas.width *ns.settings.tile_to_render[0]/ns.settings.tile_factor,
+        -ctx.canvas.height*ns.settings.tile_to_render[1]/ns.settings.tile_factor
+      )
+    }
+
+    ns.getRenderingPixelDimensions = function() {
+      let width = Math.floor(ns.mm_to_px(ns.settings.image_width))
+      let height = Math.floor(ns.mm_to_px(ns.settings.image_height))
+      return {w:width, h:height}
+    }
+
+    ns.addMissingVisualizationData = function() {
+      ns.log("Add missing visualization data...")
+      var colorIssues = 0
+      var coordinateIssues = 0
+      var g = ns.g
+      g.nodes().forEach(function(nid){
+        var n = g.getNodeAttributes(nid)
+        if (!isNumeric(n.x) || !isNumeric(n.y)) {
+          var c = getRandomCoordinates()
+          n.x = c[0]
+          n.y = c[1]
+          coordinateIssues++
+        }
+        if (!isNumeric(n.size)) {
+          n.size = 1
+        }
+        if (n.color == undefined) {
+          n.color = '#665'
+          colorIssues++
+        }
+        if (n.label == undefined) {
+          n.label = ''
+        }
+      })
+
+      if (coordinateIssues > 0) {
+        console.log('Note: '+coordinateIssues+' nodes had coordinate issues. We carelessly fixed them.')
+      }
+
+      function isNumeric(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n)
+      }
+      
+      function getRandomCoordinates() {
+        var candidates
+        var d2 = Infinity
+        while (d2 > 1) {
+          candidates = [2 * Math.random() - 1, 2 * Math.random() - 1]
+          d2 = candidates[0] * candidates[0] + candidates[1] * candidates[1]
+        }
+        var heuristicRatio = 5 * Math.sqrt(g.order)
+        return candidates.map(function(d){return d * heuristicRatio})
+      }
+      ns.report("...done.")
+    }
+
+    ns.rescaleGraphToGraphicSpace = function(options) {
+      ns.log("Rescale graph to graphic space...")
+      options = options || {}
+      options.flip_x = options.flip_x || false
+      options.flip_y = options.flip_y || false
+      options.rotate = options.rotate || 0
+      options.use_barycenter_ratio = options.use_barycenter_ratio || .2 // Between 0 (center for borders) and 1 (center for mass)
+      options.contain_in_inscribed_circle = options.contain_in_inscribed_circle || false
+      options.margin_bottom = (options.margin_bottom === undefined)?( 6):(options.margin_bottom) // in mm, space for the text etc.
+      options.margin_right  = (options.margin_right  === undefined)?( 6):(options.margin_right ) // in mm, space for the text etc.
+      options.margin_left   = (options.margin_left   === undefined)?( 6):(options.margin_left  ) // in mm, space for the text etc.
+      options.margin_top    = (options.margin_top    === undefined)?( 6):(options.margin_top   ) // in mm, space for the text etc.
+
+      var g = ns.g
+      let dim = ns.getRenderingPixelDimensions()
+      let m = {
+        t: ns.mm_to_px(options.margin_top),
+        r: ns.mm_to_px(options.margin_right),
+        b: ns.mm_to_px(options.margin_bottom),
+        l: ns.mm_to_px(options.margin_left)
+      }
+
+      ns.rescaler = []
+
+      // Flip
+      if (options.flip_x) {
+        ns.rescaler.push(xy => [-xy[0], xy[1]])
+        g.nodes().forEach(function(nid){
+          var n = g.getNodeAttributes(nid)
+          n.x = -n.x
+        })
+      }
+      if (options.flip_y) {
+        ns.rescaler.push(xy => [xy[0], -xy[1]])
+        g.nodes().forEach(function(nid){
+          var n = g.getNodeAttributes(nid)
+          n.y = -n.y
+        })
+      }
+
+      // Rotate
+      function cartesian2Polar(x, y){
+        let dist = Math.sqrt(x*x + y*y)
+        let radians = Math.atan2(y,x) //This takes y first
+        let polarCoor = { dist:dist, radians:radians }
+        return polarCoor
+      }
+      if (options.rotate != 0) {
+        let theta = Math.PI * options.rotate / 180
+        g.nodes().forEach(function(nid){
+          var n = g.getNodeAttributes(nid)
+          let pol = cartesian2Polar(n.x,n.y)
+          let d = pol.dist
+          let angle = pol.radians + theta
+          n.x = d * Math.cos(angle)
+          n.y = d * Math.sin(angle)
+        })
+        ns.rescaler.push(xy => {
+          let pol = cartesian2Polar(xy[0], xy[1])
+          let d = pol.dist
+          let angle = pol.radians + theta
+          let x = d * Math.cos(angle)
+          let y = d * Math.sin(angle)
+          return [x, y]
+        })
+      }
+
+      var ratio
+      var xcenter
+      var ycenter
+
+      // Barycenter resize
+      var xbarycenter = 0
+      var ybarycenter = 0
+      var wtotal = 0
+      var dx
+      var dy
+
+      g.nodes().forEach(function(nid){
+        var n = g.getNodeAttributes(nid)
+        // We use node size as weight (default to 1)
+        n.size = n.size || 1
+        xbarycenter += n.size * n.x
+        ybarycenter += n.size * n.y
+        wtotal += n.size
+      })
+      xbarycenter /= wtotal
+      ybarycenter /= wtotal
+
+      // Geometric center
+      let xext = d3.extent(g.nodes(), nid => g.getNodeAttribute(nid, 'x'))
+      let yext = d3.extent(g.nodes(), nid => g.getNodeAttribute(nid, 'y'))
+      var xgeocenter = (xext[0] + xext[1]) / 2
+      var ygeocenter = (yext[0] + yext[1]) / 2
+
+      // Compromise
+      xcenter = options.use_barycenter_ratio * xbarycenter + (1-options.use_barycenter_ratio) * xgeocenter
+      ycenter = options.use_barycenter_ratio * ybarycenter + (1-options.use_barycenter_ratio) * ygeocenter
+
+      if (options.contain_in_inscribed_circle) {
+        var dmax = 0 // Maximal distance from center
+        g.nodes().forEach(function(nid){
+          var n = g.getNodeAttributes(nid)
+          var d = Math.sqrt( Math.pow(n.x - xcenter - n.size, 2) + Math.pow(n.y - ycenter - n.size, 2) )
+          dmax = Math.max(dmax, d)
+        })
+
+        ratio = ( Math.min(dim.w-m.r-m.l, dim.h-m.t-m.b) ) / (2 * dmax)
+        console.log("Rescale ratio: "+ratio)
+      } else {
+        var dxmax = 0
+        var dymax = 0
+        g.nodes().forEach(function(nid){
+          var n = g.getNodeAttributes(nid)
+          var dx = Math.abs(n.x - xcenter - n.size)
+          var dy = Math.abs(n.y - ycenter - n.size)
+          dxmax = Math.max(dxmax, dx)
+          dymax = Math.max(dymax, dy)
+        })
+        ratio = Math.min((dim.w-m.r-m.l)/(2 * dxmax), (dim.h-m.t-m.b)/(2 * dymax))
+        console.log("Rescale ratio: "+ratio)
+      }
+
+      ratio = +process.env.BASEMAP_ZOOM_RATIO || 0.42 // CUSTOM: we fix it so that it is constant from one map to another (since this is 4K with about the same number of nodes)
+      
+      // Resize
+      /*g.nodes().forEach(function(nid){
+        var n = g.getNodeAttributes(nid)
+        n.x = m.l + (dim.w-m.r-m.l) / 2 + (n.x - xcenter) * ratio
+        n.y = m.t + (dim.h-m.t-m.b) / 2 + (n.y - ycenter) * ratio
+        n.size *= ratio
+      })*/
+      // CUSTOM: we resize around the barycenter
+      // First, let's center on zero.
+      let everything = {x:0, y:0, count:0}
+      g.nodes().forEach((nid,i) => {
+        const n = g.getNodeAttributes(nid)
+        everything.count++
+        everything.x += +n.x
+        everything.y += +n.y
+      })
+      everything.x /= everything.count
+      everything.y /= everything.count
+      g.nodes().forEach((nid,i) => {
+        const n = g.getNodeAttributes(nid)
+        n.x -= everything.x
+        n.y -= everything.y
+      })
+      ns.rescaler.push(xy => [xy[0]-everything.x, xy[1]-everything.y])
+
+      g.nodes().forEach(function(nid){
+        var n = g.getNodeAttributes(nid)
+        n.x = m.l + 0.5*(dim.w-m.r-m.l) + (n.x - everything.x) * ratio
+        n.y = m.t + 0.6*(dim.h-m.t-m.b) + (n.y - everything.y) * ratio
+        n.size *= ratio
+      })
+      ns.rescaler.push(xy => [m.l + 0.5*(dim.w-m.r-m.l) + (xy[0] - everything.x) * ratio, m.t + 0.6*(dim.h-m.t-m.b) + (xy[1] - everything.y) * ratio])
+
+      // // Test ns.rescaler
+      // let datum = [1,1]
+      // ns.rescaler.forEach(f => {
+      //   datum = f(datum)
+      // })
+      // console.log("Rescale [1,1]", datum)
+
+      ns.report("...done.")
+    }
+
+    ns.mm_to_px = function(d) {
+      return d * ns.settings.rendering_dpi * 0.0393701 / ns.settings.tile_factor
+    }
+
+    ns.pt_to_px = function(d) {
+      return Math.round(1000 * d * ns.settings.rendering_dpi / ( 72 * ns.settings.tile_factor )) / 1000
+    }
+
+    ns.px_to_pt = function(d) {
+      return Math.round(1000 * d * ( 72 * ns.settings.tile_factor ) / ns.settings.rendering_dpi) / 1000
+    }
+
+    //// LOG
+    ns.log = function(txt) {
+      console.log(txt)
+      ns.logTime = Date.now()
+    }
+    ns.report = function(txt) {
+      if (ns.logTime) {
+        var time = Date.now() - ns.logTime
+        time /= 1000
+        txt += " TIME: "+time+" s"
+      }
+      console.log(txt)
+      ns.logTime = Date.now()
+    }
+    ns.log2 = function(txt) {
+      console.log('\t'+txt)
+      ns.logTime2 = Date.now()
+    }
+    ns.report2 = function(txt) {
+      if (ns.logTime2) {
+        var time = Date.now() - ns.logTime2
+        time /= 1000
+        txt += " TIME: "+time+" s"
+      }
+      console.log('\t'+txt)
+      ns.logTime2 = Date.now()
+    }
+
+    return ns
+  }
+
+  return newRenderer()
+}
